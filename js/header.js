6e919decebcb63beaed1133435521062a63d46f8
DeitySenpai <deitysenpainio@gmail.com>
2021-04-23 14:27:56 +0300

    change avatar

--- js/header.js
+++ js/header.js
@@ -1,10 +1,7 @@
 const [header] = document.getElementsByTagName("header");
-const avatar = document.getElementById("avatar");
 const welcome = document.getElementById("welcome");
 
-window.onscroll = function scrollFunction() {
-	let n = (document.documentElement.scrollTop == 0 ? 1 : .9 - (document.documentElement.scrollTop-100)/900).toFixed(2);
-	avatar.style.opacity = n < 0 ? 0 : n;
-  if (document.documentElement.scrollTop > document.documentElement.clientHeight-header.offsetHeight) header.classList.add('header-scrolled'); 
-  else header.classList.remove('header-scrolled'); //for alna >>.>
-}
+window.addEventListener('scroll', () => {
+	if (document.documentElement.scrollTop > document.documentElement.clientHeight-header.offsetHeight) header.classList.add('header-scrolled');
+	else header.classList.remove('header-scrolled');
+})
